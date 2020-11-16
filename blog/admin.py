b35c526f946573782d21bc7e22b17901663f6dc7
Simeon Shopov <simeon.t.shopov@gmail.com>
2020-11-16 14:55:44 +0200

    Added blog post model and adminside functionalities.

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,9 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'created', 'updated', 'status')
+    list_filter = ('status', 'created', 'publish', 'updated')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'body')
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 class Post(models.Model):
     DRAFT = 'draft'
     PUBLISHED = 'published'
+
     choices = (
         (DRAFT, 'Draft'),
         (PUBLISHED, 'Published'),
     )
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
-    body = models.TextField()
-    image = models.ImageField(upload_to='blog/posts/images')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=80, null=False, blank=False)
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
+    body = models.TextField(null=False, blank=False)
+    image = models.ImageField(upload_to=f'blog/posts/images')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='date created')
+    updated = models.DateTimeField(auto_now=True, verbose_name='last updated')
+    publish = models.DateTimeField(default=timezone.now, verbose_name='date published')
     status = models.CharField(max_length=10, choices=choices, default=DRAFT)
 
     class Meta:
+        verbose_name = 'Blog post'
+        verbose_name_plural = 'Blog posts'
         ordering = ['-publish']
 
     def __str__(self):
         return self.title
+
+
+# Delete the image associated with the blog post on deletion of the post itself
+@receiver(post_delete, sender=Post)
+def submission_delete(sender, instance, **kwargs):
+    instance.image.delete(False)

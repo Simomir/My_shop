05adfa589596ffc28af2e6f1e007d264c7c26f83
Simeon Shopov <simeon.t.shopov@gmail.com>
2020-12-04 10:44:18 +0200

    Some cleaning up and minor changes to user html templates.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, View
